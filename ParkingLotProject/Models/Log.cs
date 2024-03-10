51cd7c16c0413edad50a4f4a83868eea39831473
Mateo Cakoni <cakoniteo@gmail.com>
2024-03-10 10:30:55 +0100

    nje koment per test

--- ParkingLotProject/Models/Log.cs
+++ ParkingLotProject/Models/Log.cs
@@ -5,6 +5,7 @@ namespace ParkingLotProject.Models
 {
     public class Log
     {
+        //ky Eshte koment i mateos
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
